00b96448b6915a0e90114ba998169d5a8d86e79c
FMalerba <malerbafede@gmail.com>
2020-12-12 14:25:14 +0100

    Cleanup

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 from skbuild import setup
-from setuptools import find_packages
 
 setup(
     name='skbuild_test',
     version='0.0.1',
-    #packages=find_packages()
     packages=['skbuild_test'],
 )
